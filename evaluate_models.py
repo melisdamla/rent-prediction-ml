ab0f4011d6f034bf9ad008cc2beefd777e19ef14
Melis Damla <melissdamlasahin@gmail.com>
2025-05-31 22:24:30 +0200

    Update evaluate_models.py

--- evaluate_models.py
+++ evaluate_models.py
@@ -24,7 +24,7 @@ except ImportError:
     exit()
 
 # Configuration
-DATA_PATH = "script/data/loyers_clean.csv"
+DATA_PATH = "data/loyers_clean.csv"
 MODEL_DIR = "models"
 RESULTS_PATH = os.path.join(MODEL_DIR, "evaluation_results.csv")
 BEST_MODEL_PATH = os.path.join(MODEL_DIR, "best_model.pkl")
